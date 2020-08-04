fb499980fc65c121151b06e44ca80bb76a938483
Will Altman <altman.120@osu.edu>
2020-08-03 20:14:00 -0400

    v0.74 Added loader

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+$(window).on("load", function() {
+    $(".loader-wrapper").fadeOut("slow");
+    $('html, body').css({
+        'overflow': 'auto',
+        'height': 'auto',
+        
+      });
+});
